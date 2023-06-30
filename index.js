31ff79609afe25104207904bd8ab414f4c352bfd
xulihang <xulihanghai@163.com>
2023-06-30 17:22:28 +0800

    implement barcode reading

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const express = require('express');
+const dbr = require("barcode4nodejs");
+dbr.initLicense("DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==");
+
 const app = express();
+app.use(express.static('public'))
+app.use(express.json({ limit: "200mb" }));
 const port = 3000;
 
-app.post('/readBarcodes', (req, res) => {
-  console.log(req);
-  res.send('Hello World!');
+app.post('/readBarcodes', async (req, res) => {
+  const startTime = (new Date()).getTime();
+  const results = await dbr.decodeBase64Async(req.body["base64"], dbr.formats.OneD | dbr.formats.PDF417 | dbr.formats.QRCode | dbr.formats.DataMatrix | dbr.formats.Aztec, "");
+  const elapsedTime = (new Date()).getTime() - startTime;
+  const response = {};
+  response.results = [];
+  for (let index = 0; index < results.length; index++) {
+    const result = results[index];
+    response.results.push({
+      barcodeText: result.value,
+      barcodeFormat: result.format,
+      x1: result.x1,
+      x2: result.x2,
+      x3: result.x3,
+      x4: result.x4,
+      y1: result.y1,
+      y2: result.y2,
+      y3: result.y3,
+      y4: result.y4
+    })
+  }
+  response.elapsedTime = elapsedTime;
+  res.json(response);
 })
-
-app.use(express.static('public'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
