1b6f5832ee8d7d73a430e514b7856c198984280a
xulihang <xulihanghai@163.com>
2023-06-30 17:53:00 +0800

    add wasm version for comparison

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
-const dbr = require("barcode4nodejs");
-dbr.initLicense("DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==");
+const dbrCPP = require("barcode4nodejs");
+dbrCPP.initLicense("t0074oQAAABz1tASEoWT4IIp00emVmVI9CDkIbZtyKBCrSbAZcltlVnqIuM6/r5afZwkP60uzSrBlN5kTWD1Y2IIawWEUYSDZAwjyIxQ=");
+let DBR = require("dynamsoft-node-barcode");
+// Please visit https://www.dynamsoft.com/customer/license/trialLicense?product=dbr&package=js&utm_source=github to get a trial license
+DBR.BarcodeReader.productKeys = 't0068NQAAAGNOq7dzkQC15+OOZbnTQto7+fNg4iMscqwrJ4Qio9JK1O4pr4daM9VTPAy5ydEvjHGhoU6EMvOU3t6R/eGof8M=';
+
+let reader = null;
+(async()=>{
+    reader = await DBR.BarcodeReader.createInstance();
+})();
 
 const app = express();
 app.use(express.static('public'))
@@ -8,8 +16,21 @@ app.use(express.json({ limit: "200mb" }));
 const port = 3000;
 
 app.post('/readBarcodes', async (req, res) => {
+  let response;
+  if (req.body["SDK"] === "DBRCPP") {
+    console.log("Use C++");
+    response = await decodeWithDBRCPP(req.body["base64"]);
+  }else{
+    console.log("Use WASM");
+    response = await decodeWithDBRWASM(req.body["base64"]);
+  }
+  
+  res.json(response);
+})
+
+async function decodeWithDBRCPP(base64){
   const startTime = (new Date()).getTime();
-  const results = await dbr.decodeBase64Async(req.body["base64"], dbr.formats.OneD | dbr.formats.PDF417 | dbr.formats.QRCode | dbr.formats.DataMatrix | dbr.formats.Aztec, "");
+  const results = await dbrCPP.decodeBase64Async(base64, dbrCPP.formats.OneD | dbrCPP.formats.PDF417 | dbrCPP.formats.QRCode | dbrCPP.formats.DataMatrix | dbrCPP.formats.Aztec, "");
   const elapsedTime = (new Date()).getTime() - startTime;
   const response = {};
   response.results = [];
@@ -29,8 +50,33 @@ app.post('/readBarcodes', async (req, res) => {
     })
   }
   response.elapsedTime = elapsedTime;
-  res.json(response);
-})
+  return response;
+}
+
+async function decodeWithDBRWASM(base64){
+  const startTime = (new Date()).getTime();
+  const results = await reader.decode("data:image/png;base64,"+base64);
+  const elapsedTime = (new Date()).getTime() - startTime;
+  const response = {};
+  response.results = [];
+  for (let index = 0; index < results.length; index++) {
+    const result = results[index];
+    response.results.push({
+      barcodeText: result.barcodeText,
+      barcodeFormat: result.barcodeFormat,
+      x1: result.localizationResult.x1,
+      x2: result.localizationResult.x2,
+      x3: result.localizationResult.x3,
+      x4: result.localizationResult.x4,
+      y1: result.localizationResult.y1,
+      y2: result.localizationResult.y2,
+      y3: result.localizationResult.y3,
+      y4: result.localizationResult.y4
+    })
+  }
+  response.elapsedTime = elapsedTime;
+  return response;
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
